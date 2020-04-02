7a46135d59034a4c2acdcbc4096594d6408a1643
pallavi-jain <pallavi.jain2610@gmail.com>
2020-04-03 00:20:18 +0530

    ts assignment 2

--- assignment-2-TS/build/services/TODOService.js
+++ assignment-2-TS/build/services/TODOService.js
@@ -9,5 +9,30 @@ class TodoService {
             setTimeout(resolve, 1000, todo_1.TODO);
         });
     }
+    saveTodo(todo) {
+        return new Promise((resolve, reject) => {
+            todo_1.TODO.push(todo);
+            setTimeout(resolve, 1000, todo_1.TODO);
+        });
+    }
+    getCompletedTodos(todo) {
+        return new Promise((resolve, reject) => {
+            let completed = todo_1.TODO.filter(function (item) {
+                item.completed === true;
+            });
+            setTimeout(resolve, 1000, completed);
+        });
+    }
+    updateTodo(todo) {
+        return new Promise((resolve, reject) => {
+            for (let i = 0; i < todo_1.TODO.length; i++) {
+                if (todo_1.TODO[i].id == todo.id) {
+                    todo_1.TODO[i] = todo;
+                    break;
+                }
+            }
+            setTimeout(resolve, 1000, todo_1.TODO);
+        });
+    }
 }
 exports.TodoService = TodoService;
--- assignment-2-TS/src/services/TodoService.ts
+++ assignment-2-TS/src/services/TodoService.ts
@@ -10,4 +10,37 @@ export class TodoService{
             
         })
     }
+
+    saveTodo(todo:TODOType):Promise<TODOType>{
+        return new Promise((resolve,reject) => {
+            TODO.push(todo);
+            setTimeout(resolve,1000,TODO);
+            
+        })
+    }
+
+    getCompletedTodos(todo:TODOType):Promise<TODOType>{
+        return new Promise((resolve,reject) => {
+            let completed = TODO.filter(function(item){
+                item.completed === true;
+            });
+
+            setTimeout(resolve,1000,completed);
+            
+        })
+    }
+
+    updateTodo(todo:TODOType):Promise<TODOType>{
+        return new Promise((resolve,reject) => {
+            for(let i =0; i < TODO.length; i++){
+                if(TODO[i].id == todo.id){
+                    TODO[i] = todo;
+                    break;
+                }
+            }
+
+            setTimeout(resolve,1000,TODO);
+            
+        })
+    }
 }
